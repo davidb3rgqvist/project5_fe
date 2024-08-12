d80b2e87941c031bc1e01e445b445c31cbf93d86
David Bergqvist <David.bergqvist@gmail.com>
2024-08-12 14:54:37 +0000

    added landing page, navbar and about page

--- healthhub/src/App.js
+++ healthhub/src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
-import './App.css';
-
 
 function App() {
     return (
         <Router>
             <NavigationBar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/login" component={Login} />
-            </Switch>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
         </Router>
     );
 }
