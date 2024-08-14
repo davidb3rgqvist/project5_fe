28e03cbb8e41339b91c4bd2a0ec6bbf5bd4a5707
David Bergqvist <David.bergqvist@gmail.com>
2024-08-14 20:34:48 +0000

    Set up server to serve React app

--- healthhub/server.js
+++ healthhub/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
